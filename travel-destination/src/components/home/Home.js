fa2f6c46f493563a5bf7c03b625d3c6ea52c03f3
Raneem Oqaily <97640655+Raneemoqaily7@users.noreply.github.com>
2022-02-14 18:22:43 +0200

    Update Home.js

--- travel-destination/src/components/home/Home.js
+++ travel-destination/src/components/home/Home.js
@@ -1,1 +1,19 @@
 
+
+import Header from '../header/Header.js'
+import data from '../../data/db.json'
+import Tours from '../tours/Tours.js' 
+import Footer from '../footer/Footer.js'
+
+
+function Home(){
+    return(
+        <>
+        <Header color ="DodgerBlue"/>
+
+       <Tours data={data}/>
+<       Footer/>
+        </>
+    )
+}
+export default Home;
